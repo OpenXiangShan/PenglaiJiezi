f928333da3946d3825330a00557c16340fcdfe5e
qianlong-zhang <qianlong2006@gmail.com>
2023-05-22 15:27:28 +0800

    cpu: no modifiction, only to invoke ci
    Change-Id: Ia896ec8bca52052e537885f6fa9d503c9c94d23d

--- run.sh
+++ run.sh
@@ -90,3 +90,5 @@ build/RISCV/gem5.opt configs/example/fs.py --caches --l2cache --enable-difftest 
 
 #maprobe
 ./build/RISCV/gem5.opt --outdir=m5out/maprobe configs/example/fs.py --xiangshan-system --enable-difftest --cpu-type=DerivO3CPU --bp-type=LTAGE --indirect-bp-type=ITTAGE --caches --cacheline_size=64 --l1i_size=128kB --l1i_assoc=8 --l1d_size=128kB --l1d_assoc=8 --l2cache --l2_size=1MB --l2_assoc=8 --l3cache --l3_size=6MB --l3_assoc=6 --mem-type=DDR4_2400_16x4 --mem-size=8GB --l2-hwp-type=SMSPrefetcher --num-cpus=1 -I=40000000 --generic-rv-cpt=/data/zql/xs-env-projects/xs-env-nanhuV2/nexus-am/apps/maprobe/build/maprobe-riscv64-xs-flash.bin --raw-cpt --difftest-ref-so /data/zql/xs-env-projects/xs-env-github/NEMU/build/riscv64-nemu-interpreter-so
+
+
